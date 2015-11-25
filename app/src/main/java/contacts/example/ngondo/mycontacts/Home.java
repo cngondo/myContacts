8ce39259968fe7f291322b4f50b1d66b00fb8c18
cngondo <devngondo@gmail.com>
2015-11-25 12:17:42 +0300

    FAB to add contacts

--- app/src/main/java/contacts/example/ngondo/mycontacts/Home.java
+++ app/src/main/java/contacts/example/ngondo/mycontacts/Home.java
@@ -2,9 +2,12 @@ package contacts.example.ngondo.mycontacts;
 
 import android.app.Activity;
 import android.app.ListActivity;
+import android.content.Context;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import com.software.shell.fab.ActionButton;
 
 public class Home extends ListActivity {
     /*Array of strings*/
@@ -13,11 +16,11 @@ public class Home extends ListActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
         /*Array adapter*/
-        setListAdapter(new myArrayAdapter(this,contactArray));
+        setListAdapter(new myArrayAdapter(this, contactArray));
 
         /*ListView listView = (ListView) findViewById(R.id.list_contacts);
         listView.setAdapter(adapter);*/
-
     }
 }
--- app/src/main/java/contacts/example/ngondo/mycontacts/myArrayAdapter.java
+++ app/src/main/java/contacts/example/ngondo/mycontacts/myArrayAdapter.java
@@ -12,15 +12,16 @@ import android.widget.TextView;
  * Created by ngondo on 11/24/15.
  */
 public class myArrayAdapter extends ArrayAdapter<String> {
+    /* Declare the values used as params in the Array adapter*/
     final Context context;
     final String[] values;
-
+//    You must have a constructor
     public myArrayAdapter(Context context, String[] values) {
         super(context, -1, values);
         this.context = context;
         this.values = values;
     }
-
+    /* Override get view for the different views in your custom view */
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         LayoutInflater layoutInflater =(LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
@@ -30,13 +31,21 @@ public class myArrayAdapter extends ArrayAdapter<String> {
         ImageView icon = (ImageView) rowView.findViewById(R.id.image);
         name.setText(values[position]);
 
+//        Populate the views with your data
         String s = values[position];
         if (s.startsWith("Ian"))
         {
             icon.setImageResource(R.drawable.ic_action);
             number.setText("084908492");
-        }else{
+        }else if(s.startsWith("John")){
             icon.setImageResource(R.drawable.ic_action);
+            number.setText("765798789");
+        }else if (s.startsWith("Corn")){
+            icon.setImageResource(R.drawable.ic_action);
+            number.setText("767898909");
+        }else if (s.startsWith("Dolly")){
+            icon.setImageResource(R.drawable.ic_action);
+            number.setText("65467980");
         }
 
         return rowView;
